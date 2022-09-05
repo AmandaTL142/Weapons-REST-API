bb3c868ef4a18772d4911aaf2ac253125bc7a55f
AmandaTL142 <82033713+AmandaTL142@users.noreply.github.com>
2022-09-05 20:39:05 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+// import express
+const express = require("express");
+// instantiate the library express
+const app = express();
+
+
+app.listen(8080, () => {
+    console.log("Server is running on port", 8080);
+});
